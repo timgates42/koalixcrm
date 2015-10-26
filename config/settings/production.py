eeb5264c6d977d3f444b5cf9dadb1008fcfea726
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-10-25 20:10:32 -0500

    [KOAL-8] Defining a cleaner setup in the production settings file.

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,7 +17,7 @@ from django.utils import six
 from .common import *  # noqa
 
 # SECRET CONFIGURATION
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
 SECRET_KEY = env("DJANGO_SECRET_KEY")
@@ -27,7 +27,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # django-secure
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 INSTALLED_APPS += ("djangosecure", )
 
 SECURITY_MIDDLEWARE = (
@@ -51,7 +51,7 @@ SESSION_COOKIE_HTTPONLY = True
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 
 # SITE CONFIGURATION
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*"]
@@ -60,7 +60,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS += ("gunicorn", )
 
 # STORAGE CONFIGURATION
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Uploaded Media Files
 # ------------------------
 # See: http://django-storages.readthedocs.org/en/latest/index.html
@@ -104,43 +104,45 @@ AWS_PRELOAD_METADATA = True
 INSTALLED_APPS = ('collectfast', ) + INSTALLED_APPS
 
 # EMAIL
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
                          default='djangocali-portal <noreply@example.com>')
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
 MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
-EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default='[djangocali-portal] ')
+EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX",
+                           default='[djangocali-portal] ')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
 # TEMPLATE CONFIGURATION
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # See:
-# https://docs.djangoproject.com/en/dev/ref/templates/api/#django.template.loaders.cached.Loader
+# https://docs.djangoproject.com/en/dev/ref/templates/api/
+    #django.template.loaders.cached.Loader
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader', ]),
 ]
 
 # DATABASE CONFIGURATION
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
 DATABASES['default'] = env.db("DATABASE_URL")
 
 # CACHING
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Heroku URL does not pass the DB number, so we parse it in
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "{0}/{1}".format(env.cache_url('REDIS_URL', default="redis://127.0.0.1:6379"), 0),
+        "LOCATION": "{0}/{1}".format(env.cache_url('REDIS_URL',
+                                     default="redis://127.0.0.1:6379"), 0),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,  # mimics memcache behavior.
-                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
         }
     }
 }
-
 
 # Your production stuff: Below this line define 3rd party library settings
