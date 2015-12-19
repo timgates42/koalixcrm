afef0dba4c97e8771446feffc8a91d0b339e52f4
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-12-19 08:55:54 -0500

    [KOAL-6] Cleaning the messy test already added

--- crm_core/tests/test_models.py
+++ crm_core/tests/test_models.py
@@ -15,8 +15,3 @@ class CustomerTestCase(TestCase):
     def test_get_customer(self):
         customer = Customer.objects.get(pk=1)
         self.assertEqual(customer.firstname, 'Honesto')
-
-'''    def test_get_absolute_url(self):
-        customer = Customer.objects.get(pk=1)
-        data = str(customer.get_absolute_url)
-        self.assertEqual(data, 'Mr Honesto Comerciante')'''
